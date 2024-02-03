27ba88082d163f26003a52155aabafe8080620ed
alaayasser01 <77371903+alaayasser01@users.noreply.github.com>
2024-02-03 22:35:57 +0200

    some edits

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 UPLOAD_FOLDER = 'savedVedio'
 ALLOWED_EXTENSIONS = {'webm'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['COUNT'] = 1  # Initialize the counter for filenames
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -23,7 +26,8 @@ def upload_file():
         return jsonify({'error': 'No selected file'})
 
     if video_file and allowed_file(video_file.filename):
-        filename = secure_filename(video_file.filename)
+        filename = f'recorded{app.config["COUNT"]}.webm'
+        app.config['COUNT'] += 1  # Increment the counter for the next file
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         video_file.save(filepath)
 
